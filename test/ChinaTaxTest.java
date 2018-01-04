5a57afb166b38907bc468ebc15692d0b4184f5c1
xieyonggang <yonggang.xie@gmail.com>
2018-01-04 11:06:38 +0800

    updated test case
    after correcting the monthly tax logic

--- test/ChinaTaxTest.java
+++ test/ChinaTaxTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
@@ -12,7 +12,7 @@ public class ChinaTaxTest {
 	public void testGetOptimumBase() throws FileNotFoundException {
 		ChinaTax taxcalc = new ChinaTax();
 		BigDecimal totalPackage = new BigDecimal("1800000");
-		BigDecimal monthlyAllowance= new BigDecimal("70000");
+		BigDecimal monthlyAllowance= new BigDecimal("60000");
 		BigDecimal optimumBase = taxcalc.getOptimumBase(totalPackage,monthlyAllowance);
 		assertEquals(0,optimumBase.compareTo(new BigDecimal("1380006.00")));
 		
@@ -22,16 +22,16 @@ public class ChinaTaxTest {
 	public void testCalculateBonusTax() {
 		ChinaTax taxcalc = new ChinaTax();
 		BigDecimal totalBonus = new BigDecimal("700000");
-		BigDecimal tax = taxcalc.calculateBonusTax(totalBonus );
+		BigDecimal tax = taxcalc.calculateBonusTax(totalBonus, true);
 		assertEquals(0,tax.compareTo(new BigDecimal("239495.00")));
 	}
 
 	@Test
 	public void testCalculateMonthlyTax() {
 		ChinaTax taxcalc = new ChinaTax();
-		BigDecimal monthlyIncome = new BigDecimal("120000");
-		BigDecimal tax = taxcalc.calculateMonthlyTax(monthlyIncome );
-		assertEquals(0,tax.compareTo(new BigDecimal("38335.00")));
+		BigDecimal taxableIncome = new BigDecimal("46668");
+		BigDecimal tax = taxcalc.calculateMonthlyTax(taxableIncome, true);
+		assertEquals(0,tax.compareTo(new BigDecimal("8870.00750")));
 	}
 
 
